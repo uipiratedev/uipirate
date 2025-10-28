24fc5d5d69327ab14a3b45db370cb86f68183971
DanishAnsari5522 <ddanish1928@gmail.com>
2025-10-28 10:47:38 +0530

    fix ui

--- screens/service/index.tsx
+++ screens/service/index.tsx
@@ -3,14 +3,18 @@ import TryBeforeCommit from "../pricing/tryBeforeCommit";
 import ServiceHero from "./hero";
 import ServicesList from "./servicesList";
 import WhatWeProvide from "./whatWeProvide";
+import PageWrapper from "@/components/PageWrapper";
+
 const Service = () => {
   return (
-    <div className="container mx-auto xl:px-32 max-md:px-4 max-xl:px-4 max-2xl:px-0 mb-12">
-      <ServiceHero />
-      <ServicesList />
-      <WhatWeProvide />
-      <TryBeforeCommit />
-    </div>
+    <PageWrapper showFloatingButton={false}>
+      <div className="container mx-auto xl:px-32 max-md:px-4 max-xl:px-4 max-2xl:px-0 mb-12">
+        <ServiceHero />
+        <ServicesList />
+        <WhatWeProvide />
+        <TryBeforeCommit />
+      </div>
+    </PageWrapper>
   );
 };
 
--- screens/serviceDetails/index.tsx
+++ screens/serviceDetails/index.tsx
@@ -8,24 +8,27 @@ import StreamlinedProcess from "./streamlinedProcess";
 import WhatWeProvide from "./whatWeProvide";
 import WhoThisIsFor from "./whoThisIsFor";
 import YouWillGet from "./youWillGet";
+import PageWrapper from "@/components/PageWrapper";
 
 const ServiceDetails = ({ data }: any) => {
   if (!data) return <div>danis...</div>;
 
   return (
-    <div>
-      <ServiceDetailsHero data={data.hero} />
+    <PageWrapper showFloatingButton={false}>
+      <div>
+        <ServiceDetailsHero data={data.hero} />
 
-      <div className="container mx-auto xl:px-32 max-md:px-4 max-xl:px-4 max-2xl:px-0">
-        <WhatWeProvide data={data.whatWeProvide} />
-        <WhoThisIsFor data={data.whoThisIsFor} />
-        <YouWillGet data={data.youWillGet} />
-        <StreamlinedProcess data={data.streamlinedProcess} />
-        <OptionalAdd data={data.optionalAdd} />
-        <LandingWork />
-        <LandingFaqs />
+        <div className="container mx-auto xl:px-32 max-md:px-4 max-xl:px-4 max-2xl:px-0">
+          <WhatWeProvide data={data.whatWeProvide} />
+          <WhoThisIsFor data={data.whoThisIsFor} />
+          <YouWillGet data={data.youWillGet} />
+          <StreamlinedProcess data={data.streamlinedProcess} />
+          <OptionalAdd data={data.optionalAdd} />
+          <LandingWork />
+          <LandingFaqs />
+        </div>
       </div>
-    </div>
+    </PageWrapper>
   );
 };
 
