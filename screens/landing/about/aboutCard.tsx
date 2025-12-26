17230a454ad898196f567043f30485972e8a9cca
Vishal <vishalanand072@gmail.com>
2025-12-27 00:14:15 +0530

    mt fixed

--- screens/landing/about/aboutCard.tsx
+++ screens/landing/about/aboutCard.tsx
@@ -124,7 +124,7 @@ const AboutCardAnimation = () => {
 
   return (
     <div className="container mx-auto px-4  pb-20 max-md:pb-12 max-md:pt-12">
-      <div className="grid grid-cols-2 gap-6 max-md:gap-4  lg:mt-16 max-lg:grid-cols-1">
+      <div className="grid grid-cols-2 gap-6 max-md:gap-4  lg:mt-40 max-lg:grid-cols-1">
         {data.map((item, index) => (
           <div
             key={index}
